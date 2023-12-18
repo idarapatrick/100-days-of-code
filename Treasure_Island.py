109d02f4fe09239a9fbd4e38c7ccf9da80746233
Idara Patrick <essienidara67@gmail.com>
2023-12-18 03:01:27 +0200

    Update Treasure_Island.py

--- Treasure_Island.py
+++ Treasure_Island.py
@@ -1,15 +1,1 @@
-one = input("Enter a number: ")
-two = input("Enter another number: ")
-sum = int(one) / int(two)
-final_sum = "{:0.4f}".format(sum)
-print("The sum is {0}".format(round(sum), 2))
-print(final_sum)
 
-check = input("do you want to see the last number of the second dogits?")
-
-if check == "yes" or check == "Yes" or check == "YES":
-    print(two[-1])
-elif check == "no":
-    print("ok")
-else:
-    print("error")
